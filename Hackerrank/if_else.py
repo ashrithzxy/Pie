35ee3fb387a2fdef234368a21862b1548c1f7847
Ashrith <35345514+ashrithzxy@users.noreply.github.com>
2018-01-23 17:04:16 +0530

    solved

--- Hackerrank/if_else.py
+++ Hackerrank/if_else.py
@@ -2,9 +2,9 @@ print("Enter a number: ")
 n = int(input())
 if (n%2==0 and 2<=n<=5):
     print("Even25")
-if (n%2==0 and 6<=n<=20):
+elif(n%2==0 and 6<=n<=20):
     print("Even620")
 elif (n%2==0 and n>20):
     print("Even20up")
-elif (n%2==1):
+elif(n%2==1):
     print("ODD")
