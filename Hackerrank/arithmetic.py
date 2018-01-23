e73f87591e56b7a22ad8bd5b4dd49f355d18e593
Ashrith <35345514+ashrithzxy@users.noreply.github.com>
2018-01-23 22:51:10 +0530

    solved

---
+++ Hackerrank/arithmetic.py
@@ -0,0 +1,8 @@
+print("Enter a: "); a = int(input())
+print("Enter b"); b = int(input())
+sum=a+b
+diff=a-b
+prod=a*b
+print("Sum is: ", sum)
+print("Difference is: ", diff)
+print("Product is: ", prod)
