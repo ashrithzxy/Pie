012f4596f0f3f0672e2b90c1c75a55a29934726d
Ashrith <35345514+ashrithzxy@users.noreply.github.com>
2018-02-05 19:32:55 +0530

    solved

--- Hackerrank/nestedlists.py
+++ Hackerrank/nestedlists.py
@@ -7,8 +7,11 @@ for outer in range(n):
     for inner in range(2):
         s = input()
         data.append(s)
-        lens = len(data)
+        lens = len(data) #length of list "data"
         for x in range(1,lens):
             data[x] = float(data[x])
     report.append(data)
-print("Nested List is: ", report)
+print("Nested List is: ", report) #code till here creates nested the nested list
+s = sorted(set([x[1] for x in report]))
+for name in sorted(x[0] for x in report if x[1] == s[1]):
+    print(name) 
