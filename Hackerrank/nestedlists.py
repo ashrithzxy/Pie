3c928996488e40d6ab9ffd1c4815331136b9c8d0
Ashrith <35345514+ashrithzxy@users.noreply.github.com>
2018-02-05 18:18:07 +0530

    half done

---
+++ Hackerrank/nestedlists.py
@@ -0,0 +1,22 @@
+print("Enter the number of student records: ")
+n = int(input())
+report = []
+print("Enter student details: ")
+for outer in range(n):
+    data = []
+    for inner in range(2):
+        s = input()
+        data.append(s)
+        lens = len(data)
+        for x in range(1,lens):
+            data[x] = float(data[x])
+    report.append(data)
+print("Nested List is: ", report)
+
+
+
+
+
+#        data.append(int(input()))
+#    report.append(data)
+#print(report)
