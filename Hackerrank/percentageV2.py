874398674136e12752dbe7ae1db49bc0d5bc47af
Ashrith <35345514+ashrithzxy@users.noreply.github.com>
2018-02-06 22:21:04 +0530

    solved

---
+++ Hackerrank/percentageV2.py
@@ -0,0 +1,14 @@
+n = int(input("Enter the number of records you want to enter: "))
+student_marks = {}
+for _ in range(n):
+    name, *line = input().split() #*line is  extended iterable unpacking, refer: bit.ly/2BYwykW
+    scores = list(map(float, line))
+    student_marks[name] = scores
+query_name = input("Enter the student you want to know the percentage of: ")
+if query_name in student_marks:
+    a = student_marks[query_name]
+else:
+    print("User not found!")
+total = sum(a)
+percentage = (total)/3
+print('%.2f' %percentage)
