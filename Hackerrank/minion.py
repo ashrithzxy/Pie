ea6c7ea10724523a0f3cc27b5c26aba399c4a167
Ashrith <35345514+ashrithzxy@users.noreply.github.com>
2018-02-15 19:13:53 +0530

    working with strings

---
+++ Hackerrank/minion.py
@@ -0,0 +1,18 @@
+def game_logic(s):
+#KEVIN: VOWELS {A,E,I,O,U}
+    vowels = 'AEIOU'
+    kevin = 0
+    stuart = 0
+    for i in range(len(s)):
+        if s[i] in vowels:
+            kevin = kevin + (len(s)-i)
+        else:
+            stuart = stuart + (len(s)-i)
+    if kevin > stuart:
+        print("Kevin",kevin)
+    elif stuart > kevin:
+        print("Stuart",stuart)
+    else:
+        print("Draw")
+s = input()
+game_logic(s)
