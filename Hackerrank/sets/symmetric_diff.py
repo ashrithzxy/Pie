0f5e94faa5ca809c6f90a457652e7281aa51e5bb
Ashrith <35345514+ashrithzxy@users.noreply.github.com>
2018-02-16 16:46:24 +0530

    HackerRank exercises

--- Hackerrank/sets/symmetric_diff.py
+++ Hackerrank/sets/symmetric_diff.py
@@ -1,7 +1,16 @@
-
 m = int(input())
 mset = set(map(int, input().split()))
 n = int(input())
 nset = set(map(int, input().split()))
 result = mset.symmetric_difference(nset)
 print(result)
+
+#-----------for printing on different lines
+#m = int(input())
+#mset = set(map(int, input().split()))
+#n = int(input())
+#nset = set(map(int, input().split()))
+#result = list(mset.union(nset) - mset.intersection(nset))
+#result.sort()
+#for i in range(len(result)):
+#    print(result[i])
