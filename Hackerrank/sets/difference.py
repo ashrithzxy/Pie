b8a14d8f3e30ffcdc405de24b4067cbcd55cfb31
Ashrith <35345514+ashrithzxy@users.noreply.github.com>
2018-02-16 21:08:08 +0530

    HackerRank exerises

---
+++ Hackerrank/sets/difference.py
@@ -0,0 +1,6 @@
+num_english = int(input())
+english = set(map(int, input().split()))
+num_french = int(input())
+french = set(map(int, input().split()))
+total = len(english.difference(french))
+print(total)
