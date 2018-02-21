550890664b5737393ca29f96e02b2c7cc4f29d90
Ashrith <35345514+ashrithzxy@users.noreply.github.com>
2018-02-21 23:15:14 +0530

    [INCOMPLETE] Two player rock, paper, scissor game.

---
+++ practicepython.org/rockpaperscissors.py
@@ -0,0 +1,31 @@
+'''
+Make a two-player Rock-Paper-Scissors game.
+(Hint: Ask for player plays (using input), compare them,
+print out a message of congratulations to the winner,
+and ask if the players want to start a new game)
+'''
+print("WELCOME TO THE GAME OF ROCK, PAPER, SCISSORS!!")
+Final_score = int(input("Enter final score: "))
+for i in Final_score:
+    player_ONE = input("Make your move: ")
+    player_TWO = input("Make your move: ")
+    quit = input("Type 'Quit' if you'd like to quit and tally: ")
+    while(quit != "Quit"):
+        if (player_ONE == "R" and player_TWO == " "):
+            #LOGIC
+        elif (player_ONE == "R" and player_TWO == " "):
+            #LOGIC
+        elif (player_ONE == "R" and player_TWO == " "):
+            #LOGIC
+        elif (player_ONE == "P" and player_TWO == " "):
+            #LOGIC
+        elif (player_ONE == "P" and player_TWO == " "):
+            #LOGIC
+        elif (player_ONE == "P" and player_TWO == " "):
+            #LOGIC
+        elif (player_ONE == "S" and player_TWO == " "):
+            #LOGIC
+        elif (player_ONE == "S" and player_TWO == " "):
+            #LOGIC
+        elif (player_ONE == "S" and player_TWO == " "):
+            #LOGIC
