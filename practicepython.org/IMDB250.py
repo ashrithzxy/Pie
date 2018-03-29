9c05aabe7f69dc209142b2792abf30b03a7b7760
Ashrith <35345514+ashrithzxy@users.noreply.github.com>
2018-03-29 11:58:41 +0530

    Update IMDB250.py

--- practicepython.org/IMDB250.py
+++ practicepython.org/IMDB250.py
@@ -7,12 +7,4 @@ r = requests.get("http://www.imdb.com/chart/top")
 r_html = r.text
 soup = BeautifulSoup(r_html)
 for titleColumn in soup.find_all("td", class_ ="titleColumn"):
-    #if titleColumn.a:
-    #open_file = open('IMDB250.txt', 'w')
-    #open_file.write(titleColumn.a.text)
-    #open_file.close()
-    #with open('IMDB250.txt', 'w') as open_file:
-        #open_file.write(titleColumn.a.text)
     print(titleColumn.a.text)
-    #else:
-    #print(titleColumn.contents[0].strip())
