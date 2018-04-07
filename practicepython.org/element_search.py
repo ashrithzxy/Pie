18e68240528adf57e50cf6a9bd50961eaf1ef224
Ashrith <35345514+ashrithzxy@users.noreply.github.com>
2018-04-07 23:37:54 +0530

    Binary search

--- practicepython.org/element_search.py
+++ practicepython.org/element_search.py
@@ -7,10 +7,21 @@ The function decides whether or not the
 given number is inside the list and returns
 (then prints) an appropriate boolean.
 '''
-def binary(my_list):
-
+def binary(my_list,n):
+    length = len(my_list)
+    mid = length/2
+    if(n>mid):
+        del my_list[0:mid]
+        binary(my_list,n)
+    elif(n<mid):
+        del my_list[mid:length]
+        binary(my_list,n)
+    elif(n==mid):
+        return True
+    else:
+        return False
 import random
 my_list = sorted(random.sample(range(1,50), 15))
-n = input("Enter the number you want searched: ")
-search_result = binary(my_list)
+n = int(input("Enter the number you want searched: "))
+search_result = binary(my_list,n)
 print(search_result)
